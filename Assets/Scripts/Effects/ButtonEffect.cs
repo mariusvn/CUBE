60bc9c92a31162560ccaf744a13518674a92ac2b
Marius Van Nieuwenhuyse <mariusvnspam@gmail.com>
2021-11-10 01:34:40 +0100

    Adding ButtonEffect

---
+++ Assets/Scripts/Effects/ButtonEffect.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEditor;
+using UnityEngine;
+
+public class ButtonEffect : MonoBehaviour
+{
+    public bool IsActive => _goInside > 0;
+    public string targetTag = "Player";
+
+    private int _goInside = 0;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            _goInside++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            _goInside--;
+        }
+    }
+}

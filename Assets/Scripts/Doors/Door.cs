c4c606cb3a839f5636dfec3fe9338282b6d15c3e
f_chavagnieux <florian.chavagnieux@epitech.eu>
2021-11-11 17:52:15 +0100

    Added Prefab PivotDoor and SlidingDoor

---
+++ Assets/Scripts/Doors/Door.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public float animDuration = 1.0f;
+    public GameObject door;
+
+    protected float _animStart = 0.0f;
+    protected bool isOpen = false;
+    protected bool inUse = false;
+    public bool IsOpen { get => isOpen; }
+
+    virtual protected IEnumerator OpenDoor()
+    {
+        print("Start Coroutine");
+        door.SetActive(false);
+        inUse = false;
+        isOpen = true;
+        yield return null;
+    }
+
+    virtual protected IEnumerator CloseDoor()
+    {
+        print("Start Coroutine");
+        door.SetActive(true);
+        inUse = false;
+        isOpen = false;
+        yield return null;
+    }
+
+    public void UseDoor()
+    {
+        if (inUse)
+            return;
+        inUse = true;
+        if (isOpen)
+            StartCoroutine("CloseDoor");
+        else
+            StartCoroutine("OpenDoor");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
